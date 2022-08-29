543bec4c96cb4c349ecc65d1705fbb2060a2a54d
Hamid Bromand <bromandhh@gmail.com>
2022-08-29 14:38:13 -0700

    Second commit

--- src/gitHub/gitAndGitHub.java
+++ src/gitHub/gitAndGitHub.java
@@ -4,7 +4,8 @@ public class gitAndGitHub {
 	
 	public static void main (String [] args) {
 		
-		System.out.println("Today we learn gitHub");
+		System.out.println("Today we learn gitHub.");
+		System.out.println("Today is a very good day.");
 	}
 
 }
